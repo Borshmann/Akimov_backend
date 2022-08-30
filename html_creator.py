38955750eb30be6f97a4409f309802c399f0736c
Borshmann <111072730+Borshmann@users.noreply.github.com>
2022-08-30 11:44:58 +0300

    Add files via upload
    Модуль создания HTML кода для рассылки

---
+++ html_creator.py
@@ -0,0 +1,119 @@
+image_start= '''<th><img src="https://daryvolkhvov.ru/items/'''
+image_end = '''"width="50px"></th>'''
+th = "</th>"
+lorem = """Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam condimentum nunc sit amet cursus molestie. Phasellus metus justo, bibendum ac faucibus quis, consequat sed quam. Fusce sit amet tellus bibendum diam blandit porta ac vel massa. Nam elementum lorem nec malesuada accumsan. Aenean eros nibh, venenatis vel neque ac, venenatis bibendum diam. Vivamus iaculis, dolor sit amet fermentum facilisis, nisi neque tincidunt tellus, eu accumsan enim sem quis lectus. Sed efficitur luctus nunc. Sed volutpat finibus odio, eu molestie lorem pretium eget. Ut pretium volutpat finibus. """
+line = """<hr align="center" width="100%" size="1" color="#1a1a1a" />"""
+body_1 = """<!doctype html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Спасибо за заказ!</title>
+  </head>
+  <body style="background-color: #ededed;font-family: sans-serif;-webkit-font-smoothing: antialiased;font-size: 14px;line-height: 1.4;margin: 0;padding: 0;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;">
+    <span style="color: transparent;display: none;height: 0;max-height: 0;max-width: 0;opacity: 0;overflow: hidden;mso-hide: all;visibility: hidden;width: 0;">Вы получили это письмо от компании © «Торговый дом «Акимов»</span>
+    <table role="presentation" border="0" cellpadding="0" cellspacing="0" style="background-color: #ededed;width: 100%;">
+      <tr>
+        <td>&nbsp;</td>
+        <td style="display: block;margin: 0 auto !important;max-width: 580px;padding: 10px;width: 580px;">
+          <div style="box-sizing: border-box;display: block;margin: 0 auto;max-width: 580px;padding: 10px;">
+            <table role="presentation" style="border-collapse: separate; mso-table-lspace: 0pt;mso-table-rspace: 0pt;width: 100%; background: #FFBA80; border-radius: 15px 15px 0 0; width: 100%; box-shadow: 5px 5px 10px grey;">
+              <tr>
+                <td style="font-family: sans-serif;font-size: 14px; vertical-align: top; box-sizing: border-box; padding: 5px;">
+                  <img src="https://daryvolkhvov.ru/static/media/logo-white.9f0026d3ff23699689f3.svg" width="150" style="display: block;margin-left: auto;margin-right: auto;padding-top: 5px;padding-bottom: 5px; width: 130px;">
+                </td>
+              </tr>
+            </table>
+
+            <table role="presentation" style="background: #FFFFFF; border-radius: 0px;width: 100%;box-shadow: 5px 5px 10px grey;">
+              <tr>
+                <td style="box-sizing: border-box; padding: 20px;">
+                    <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+                      <h4 style="color: #black;font-family: 'Trebuchet MS', sans-serif;font-weight: 400;line-height: 1.4;margin-bottom: 2px;margin-top: 0px;" align="left">Здравствуйте, """
+body_2 = """!</h4>
+                      <br>
+                      <p style="color: #696969;font-family: 'Tahoma', sans-serif;font-size: 11px;font-weight: normal;margin: 0;margin-bottom: 1px;">Благодарим за заказ в Иконотеке «Дары волхвов».<br>В ближайшее время с Вами свяжется персональный менеджер, у которого будет вся информация о статусе Вашего заказа.</p>
+                    </table>
+                  <br>
+                  <table role="presentation" border="0" cellpadding="5" cellspacing="5" style="width: 100%;">
+                    <tr>
+                      <th><h4 style="color: #black;font-family: 'Tahoma', sans-serif;font-size: 14px;font-weight: normal;margin: 0;margin-bottom: 0px;margin-top: 0px;"align="left">Заказ № """
+body_3 = """</h4></th>
+                      <th><h4 style="color: #black;font-family: 'Tahoma', sans-serif;font-size: 14px;font-weight: normal;margin: 0;margin-bottom: 0px;margin-top: 0px;"align="right">Дата заказа: """
+body_4 = """</h4></th>
+                    </tr>
+                  </table>
+                  <hr style="border: 0;border-bottom: 1px solid #808080;margin: 0;" align="center" width="100%" size="1"/>
+                  <br>
+                  <table role="presentation" border="0" cellpadding="5" cellspacing="5" style="width: 100%;">
+                    <tr>
+                      <th style="color: #black;font-family: 'Tahoma', sans-serif;font-size: 13px;font-weight: normal;margin: 0;margin-bottom: 0px;margin-top: 0px;"align="left">Покупатель: </th>
+                      <th style="color: #black;font-family: 'Tahoma', sans-serif;font-size: 13px;font-weight: normal;margin: 0;margin-bottom: 0px;margin-top: 0px;"align="left">"""
+body_5 = """</th>
+                    </tr>
+                    <tr>
+                      <th style="color: #black;font-family: 'Tahoma', sans-serif;font-size: 13px;font-weight: normal;margin: 0;margin-bottom: 0px;margin-top: 0px;"align="left">Способ доставки: </th>
+                      <th style="color: #black;font-family: 'Tahoma', sans-serif;font-size: 13px;font-weight: normal;margin: 0;margin-bottom: 0px;margin-top: 0px;"align="left">Курьерская доставка по адресу: <br>Москва, Кремль, кабинет 23</th>
+                    </tr>
+                    <tr>
+                      <th style="color: #black;font-family: 'Tahoma', sans-serif;font-size: 13px;font-weight: normal;margin: 0;margin-bottom: 0px;margin-top: 0px;"align="left">Способ оплаты: </th>
+                      <th style="color: #black;font-family: 'Tahoma', sans-serif;font-size: 13px;font-weight: normal;margin: 0;margin-bottom: 0px;margin-top: 0px;"align="left">Оплата курьеру</th>
+                    </tr>
+                  </table>
+                  <br>
+                  <table>
+                    <tr>
+                      <th align="left"><h4 style="color: #black;font-family: 'Tahoma', sans-serif;font-size: 14px;font-weight: normal;margin: 0;margin-bottom: 0px;margin-top: 0px;"align="left">Изделия в заказе:</h4></th>
+                    </tr>
+                  </table>
+                  <hr style="border: 0;border-bottom: 1px solid #808080;margin: 0;"  align="center" width="100%" size="1"/>
+                  <br>
+                  <table role="presentation" border="0" cellpadding="5" cellspacing="0" style="width: 100%;">
+                    <tr>
+                      <th><p style="color: #696969;font-family: 'Verdana', sans-serif;font-size: 11px;font-weight: normal;margin: 0;margin-bottom: 1px;">Фото</p></th>
+                      <th align="left"><p style="color: #696969;font-family: 'Verdana', sans-serif;font-size: 11px;font-weight: normal;margin: 0;margin-bottom: 1px;">Название</p></th>
+                      <th><p style="color: #696969;font-family: 'Verdana', sans-serif;font-size: 11px;font-weight: normal;margin: 0;margin-bottom: 1px;">Кол-во</p></th>
+                      <th><p style="color: #696969;font-family: 'Verdana', sans-serif;font-size: 11px;font-weight: normal;margin: 0;margin-bottom: 1px;">Цена 1 шт.</p></th>
+                      <th><p style="color: #696969;font-family: 'Verdana', sans-serif;font-size: 11px;font-weight: normal;margin: 0;margin-bottom: 1px;">Общая цена</p></th>
+                    </tr>"""
+body_6 = """                    </table><table role="presentation" border="0" cellpadding="5" cellspacing="0" style="width: 100%;">
+                    <tr>
+                      <th align="right" style="color: #black;font-family: 'Tahoma', sans-serif;font-size: 13px;font-weight: normal;margin: 0;margin-bottom: 0px;margin-top: 0px;">Стоимость заказа:
+"""
+
+body_7 = """руб</th>
+                    </tr>
+                  </table>
+                  <br>
+                  <table>
+                    <br>
+                    <tr>
+                      <th align="left" ><p style="color: #696969;font-family: 'Tahoma', sans-serif;font-size: 11px;font-weight: normal;margin: 0;margin-bottom: 1px;">Звоните, если возникли какие-то вопросы:<br>8 (812) 123-45-67<br>8 (812) 123-34-78<br></p></th>
+                    </tr>
+                  </table>
+                </td>
+              </tr>
+
+            <!-- END MAIN CONTENT AREA -->
+            </table>
+            <!-- END CENTERED WHITE CONTAINER -->
+
+            <!-- START FOOTER -->
+            <div style="clear: both;margin-top: 0px;text-align: center;width: 100%;">
+              <table role="presentation" border="0" cellpadding="0" cellspacing="0" style="width: 100%;">
+                <tr>
+                  <td style="padding-bottom: 10px;padding-top: 10px;background-color: #262626;border-radius: 0 0 15px 15px;">
+                    <span style="color: #d4d4d4;font-family: 'Tahoma', sans-serif;font-size: 11px;font-weight: normal;margin: 0;margin-bottom: 1px;">«Торговый дом «Акимов» © <br> 199106, г. Санкт-Петербург, 19 линия В.О., д. 32, корп. 6, лит. Л<br>© 2022 — 2023</span>
+                  </td>
+                </tr>
+              </table>
+            </div>
+            <!-- END FOOTER -->
+
+          </div>
+        </td>
+        <td>&nbsp;</td>
+      </tr>
+    </table>
+  </body>
+</html>"""
